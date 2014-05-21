bffdf99039a2d6191c7031ed2a182ed46e6d09bf
Jennifer L Brown <jlynn81@fullsail.edu>
2014-05-20 21:18:06 -0500

    refactored file names

--- index.php
+++ index.php
@@ -10,12 +10,12 @@ $pagename = 'index';
 $views = new productViewModel();
 $products = new productModel();
 
-$views->getView("views/current_products.php");
+$views->getView("views/current_products.html");
 
 if(!empty($_GET["action"])){
     if($_GET["action"]=="product_details"){
         $result = $products->getOne($_GET["item"]);
-        $views->getView("application/views/current_products.php", $result);
+        $views->getView("application/views/current_products.html", $result);
     }
 }else{
     $result = $products->getAll();
@@ -23,7 +23,7 @@ if(!empty($_GET["action"])){
 }
 
 //display page content
-$views->getView("views/current_products.php");
+$views->getView("views/current_products.html");
 
 
 
